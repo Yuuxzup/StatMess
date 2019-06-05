8dffade2a11bca04bd62a0856176766ab268d7ff
Yuuxzup <hugop77310@hotmail.fr>
2019-06-05 13:35:17 +0200

    carousel et BDD

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ import { GlobalService } from './global.service';
 export class AppComponent implements OnInit {
   isReady=false;
   isCguVisible=false;
+  timer = 4000;
 
   
 
@@ -19,6 +20,57 @@ export class AppComponent implements OnInit {
   ngOnInit(){
     this.globalService.defineDB()
     console.log("BD on process : "+this.globalService.nameDB)
+  }
+
+  ngAfterViewInit(){
+
+  var timer = this.timer;
+  var i = 0;
+  var max = document.getElementById('c').getElementsByTagName("li").length;
+ 
+	document.getElementById('c').getElementsByTagName("li")[i].style.opacity="1";
+  document.getElementById('c').getElementsByTagName("li")[i].style.left="0";
+
+  document.getElementById('c').getElementsByTagName("li")[i+1].style.opacity="1";
+  document.getElementById('c').getElementsByTagName("li")[i+1].style.left="75%";
+  
+  setInterval(function(){ 
+    
+
+		document.getElementById('c').getElementsByTagName("li")[0].style.opacity="0";
+    document.getElementById('c').getElementsByTagName("li")[1].style.opacity="0";
+    document.getElementById('c').getElementsByTagName("li")[2].style.opacity="0";
+    document.getElementById('c').getElementsByTagName("li")[3].style.opacity="0";
+    document.getElementById('c').getElementsByTagName("li")[4].style.opacity="0";
+    document.getElementById('c').getElementsByTagName("li")[5].style.opacity="0";
+    document.getElementById('c').getElementsByTagName("li")[6].style.opacity="0";
+    document.getElementById('c').getElementsByTagName("li")[7].style.opacity="0";
+    document.getElementById('c').getElementsByTagName("li")[8].style.opacity="0";
+    document.getElementById('c').getElementsByTagName("li")[9].style.opacity="0";
+
+
+    document.getElementById('c').getElementsByTagName("li")[i].style.transitionDelay='0.25s';
+    document.getElementById('c').getElementsByTagName("li")[i+1].style.transitionDelay='0.5s'; //0.50
+
+		if (i < max-2) {
+			i = i+2; 
+		}
+
+		else { 
+			i = 0; 
+		}  
+
+    document.getElementById('c').getElementsByTagName("li")[i].style.opacity="1";
+    document.getElementById('c').getElementsByTagName("li")[i].style.left="0";
+    document.getElementById('c').getElementsByTagName("li")[i].style.transitionDelay='1.25s';
+
+    document.getElementById('c').getElementsByTagName("li")[i+1].style.opacity="1";
+    document.getElementById('c').getElementsByTagName("li")[i+1].style.left="25%";
+    document.getElementById('c').getElementsByTagName("li")[i+1].style.transitionDelay='1.5s';
+    
+	
+	}, timer);
+    
   }
 
   launch(){
