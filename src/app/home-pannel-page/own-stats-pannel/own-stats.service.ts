56c36ea709c443b461ae772c1e80a2e626d466cb
Yuuxzup <hugop77310@hotmail.fr>
2019-06-22 12:28:23 +0200

    participant string

--- src/app/home-pannel-page/own-stats-pannel/own-stats.service.ts
+++ src/app/home-pannel-page/own-stats-pannel/own-stats.service.ts
@@ -387,7 +387,7 @@ export class OwnStatsService {
     let setKeyInBaseGenre = new Set(Object.keys(baseGenre))
     let setKeyGenreF= new Set(baseGenre2["f"])
     let setKeyGenreM= new Set(baseGenre2["m"])
-    setInterlocuteurs.forEach(function(participant){
+    setInterlocuteurs.forEach(function(participant:string){
       let prenom1 = participant.split(" ")[0].toLowerCase()
       let prenom2 = participant.split(" ")[0].replace("é","e").replace("è","e").replace("ê","e").replace("ë","e").replace("ï","i").replace("î","i").replace("É","E")
         if(setKeyInBaseGenre.has(prenom1)){
