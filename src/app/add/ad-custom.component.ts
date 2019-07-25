8ddd122214e8e9b4b16b8a4289ef67b0d8f1f613
Paul TONDEREAU <paul.tondereau@yahoo.fr>
2019-07-25 20:02:49 +0200

    Test pub Hugo

--- src/app/add/ad-custom.component.ts
+++ src/app/add/ad-custom.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit} from '@angular/core';
+import { Component, Input, OnInit, AfterViewInit} from '@angular/core';
 
 
 
@@ -7,15 +7,30 @@ import { Component, Input, OnInit} from '@angular/core';
   templateUrl: './ad-custom.component.html',
   styleUrls: [ './ad-custom.component.css' ]
 })
-export class AdCustomComponent implements OnInit {
+export class AdCustomComponent implements OnInit, AfterViewInit {
+
     @Input() urlAddon:any;
     url:any
   constructor() {}
 
   ngOnInit(){
     var addon = this.urlAddon==2 ? "Double" : "One";
-    this.url="http://ptondereau.perso.centrale-marseille.fr/add"+addon+".html";
+    this.url="http://ptondereau.perso.centrale-marseille.fr/add"+addon+"Bis.html";
   }
+
+  ngAfterViewInit(){
+    console.log("test")
+    this.createIframe();
+  }
+
+  createIframe(){
+    var i = document.createElement("iframe");
+    i.src = this.url;
+    i.scrolling = "auto";
+    i.width = "100%";
+    i.height = "300px";
+    document.getElementById("div-that-holds-the-iframe").appendChild(i);
+  };
 }
 
 
